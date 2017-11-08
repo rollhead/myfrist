5b9323848b841a46b7d412db6b7932b08966dd04
rollhead <577492069@qq.com>
2017-11-08 10:44:17 +0800

    远程提交删除

--- bank/src/main/java/com/bank/controller/UserController.java
+++
@@ -1,7 +0,0 @@
-package com.bank.controller;
-
-/**
- * Created by Administrator on 2017/11/8 0008.
- */
-public class UserController {
-}
--- bank/src/main/java/com/bank/entity/Test.java
+++
@@ -1,7 +0,0 @@
-package com.bank.entity;
-
-/**
- * Created by Administrator on 2017/11/8 0008.
- */
-public class Test {
-}
